5e6d5d520114d91783c20400e3c682ce8bcbd8bd
ytefera <you@example.com>
2019-03-17 02:52:01 -0700

    Updates

---
+++ precache-manifest.bc28bb619358b272729256b947cccf02.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/roots/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "e62734e2ab1c07fd5344",
+    "url": "/roots/static/js/runtime~main.74d8b403.js"
+  },
+  {
+    "revision": "03da65e9e4384424dbd8",
+    "url": "/roots/static/js/main.8e14d96a.chunk.js"
+  },
+  {
+    "revision": "ba264cd90c4d36546d5a",
+    "url": "/roots/static/js/2.8f71a45e.chunk.js"
+  },
+  {
+    "revision": "03da65e9e4384424dbd8",
+    "url": "/roots/static/css/main.a9953acb.chunk.css"
+  },
+  {
+    "revision": "19225193415e591dbd8f48337dc2a1fb",
+    "url": "/roots/index.html"
+  }
+];
